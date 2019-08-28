2d5cacdb0467dd039aec82e18e7c6cad807119c7
Michael Kroon <kroonology@gmail.com>
2019-08-27 18:22:10 -0600

    CRUD appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,49 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:show, :edit, :update]
+
   def index
+    @appointments = current_user.accounts
   end
 
   def show
   end
 
   def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = current_user.appointments.new(appointment_params)
+    if @appointment.save
+      redirect_to appointments_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @appointment.update(appointment_params)
+      redirect_to appointments_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+    redirect_to appointments_path
+  end
+
+  private
+    def appointment_params
+      params.require(:appointment).permit(:weekday)
+    end
+
+    def set_appointment
+      @appointment = current_user.appointments.find(params[:id])
+    end
+
 end
